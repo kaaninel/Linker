f54db1b1db44df836aa274e434bd2a00f01b22f2
kaan <kaaninel@gmail.com>
2019-03-12 17:04:37 +0300

    linker update command

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const path = require('path');
 if(args.length) {
   if(args[0] === "create" && args[1]) {
     execSync(`cp -a ${path.resolve(__dirname ,"./Proto")}/. ${process.cwd()}/${args[1]}`);
+  } else if (args[0] === "update") {
+    execSync(`rm -r ${process.cwd()}/Source/Link && cp -a ${path.resolve(__dirname ,"./Proto/Source/Link")}/. ${process.cwd()}/Source/Link`);
   }
 } else {
   require("./Dist/Compiler.js");
